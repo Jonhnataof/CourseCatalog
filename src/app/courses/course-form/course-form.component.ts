3eb6d48e2e8a4760feb3ffdba99a558e0636e089
Jonhnata de Oliveira <jonhnataof@gmail.com>
2024-07-22 10:49:41 -0300

    Criação do component do formulario.

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-course-form',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar,
+    private location: Location
+  ) { 
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+    
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    this.service.save(this.form.value)
+    .subscribe(result => this.onSuccess(), error => this.onError());
+  }
+
+  onCancel(){
+    this.location.back();
+  }
+
+  private onSuccess(){
+    this.snackBar.open('Salvo com sucesso!', '', {duration: 3000});
+    this.onCancel();
+  }
+
+  private onError(){
+    this.snackBar.open('Erro ao salvar curso.', '', {duration: 3000});
   }
 
 }
